868674bdedea24db7dd895b90766b1229816f1ce
Robert Rice <robertrice12009@yahoo.com>
2022-04-11 15:08:24 -0500

    this is my first push

---
+++ src/day1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class day1 {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the store number" );
+        int store1 = scan.nextInt();
+        System.out.println("Please enter the cube for " + store1 );
+        int store1cube = scan.nextInt();
+        System.out.println("Please enter the weight for " + store1);
+        int store1weight = scan.nextInt();
+        System.out.println("Is there another store to enter, yes or no?");
+        String yesno = scan.next();
+
+
+    }
+}
